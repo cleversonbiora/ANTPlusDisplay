4e995ff0fe3d9298635fc5e80b6da76d9abad068
Cleverson Biora <cleverson.biora@gmail.com>
2020-06-19 09:53:47 -0300

    first commit

---
+++ ANT Display/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ANT_Display
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblVel = new System.Windows.Forms.Label();
+            this.lblCad = new System.Windows.Forms.Label();
+            this.lblDist = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Velocidade:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(317, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cadência:";
+            // 
+            // lblVel
+            // 
+            this.lblVel.AutoSize = true;
+            this.lblVel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVel.Location = new System.Drawing.Point(12, 49);
+            this.lblVel.Name = "lblVel";
+            this.lblVel.Size = new System.Drawing.Size(139, 46);
+            this.lblVel.TabIndex = 2;
+            this.lblVel.Text = "0 km/h";
+            // 
+            // lblCad
+            // 
+            this.lblCad.AutoSize = true;
+            this.lblCad.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCad.Location = new System.Drawing.Point(317, 49);
+            this.lblCad.Name = "lblCad";
+            this.lblCad.Size = new System.Drawing.Size(42, 46);
+            this.lblCad.TabIndex = 3;
+            this.lblCad.Text = "0";
+            // 
+            // lblDist
+            // 
+            this.lblDist.AutoSize = true;
+            this.lblDist.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDist.Location = new System.Drawing.Point(15, 181);
+            this.lblDist.Name = "lblDist";
+            this.lblDist.Size = new System.Drawing.Size(42, 46);
+            this.lblDist.TabIndex = 5;
+            this.lblDist.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(15, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 25);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Distancia:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 450);
+            this.Controls.Add(this.lblDist);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblCad);
+            this.Controls.Add(this.lblVel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblVel;
+        private System.Windows.Forms.Label lblCad;
+        private System.Windows.Forms.Label lblDist;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ANT Display/Form1.cs
@@ -0,0 +1,77 @@
+﻿using ANT.Framework;
+using ANT.Framework.DataSources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace ANT_Display
+{
+    public partial class Form1 : Form
+    {
+        public ANTManger antMgr;
+        System.Timers.Timer updateTimer = new System.Timers.Timer(1000);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+             antMgr = new ANTManger();
+            
+            foreach (ANTManger.AntPlus_Connection i in antMgr.deviceList)
+            {
+                Debug.WriteLine(i.dataSource.getCurrentDistance().ToString());
+            }
+            updateTimer.Elapsed += new System.Timers.ElapsedEventHandler(updateTimer_Tick);
+            updateTimer.Start();
+        }
+
+        private void updateTimer_Tick(object sender, ElapsedEventArgs e)
+        {
+            var lastSeenData = antMgr.deviceList[0].dataSource.getLastDataRcvd();
+            var lastSeenData2 = antMgr.deviceList[1].dataSource.getLastDataRcvd();
+            ReceberMensagem(lastSeenData, lastSeenData2);
+            //foreach (ANTManger.AntPlus_Connection i in antMgr.deviceList)
+            //{
+            //    if(i.connectedChannel != null)
+            //    {
+            //        Debug.WriteLine(i.dataSource.getCurrentDistance().ToString());
+            //    }
+            //}
+        }
+
+        delegate void ReceberMensagemCallback(DataSourcePacket lastSeenData, DataSourcePacket lastSeenData2);
+        void ReceberMensagem(DataSourcePacket lastSeenData, DataSourcePacket lastSeenData2)
+        {
+            if (InvokeRequired)
+            {
+                ReceberMensagemCallback callback = ReceberMensagem;
+                Invoke(callback, lastSeenData, lastSeenData2);
+            }
+            else
+            {
+                if (lastSeenData != null)
+                {
+                    lblDist.Text = Math.Round(lastSeenData.distance).ToString()+ " m";
+                    lblVel.Text = Math.Round(lastSeenData.speed_ms * 3.6).ToString() + " km/h";
+                }
+                if (lastSeenData2 != null)
+                {
+                    lblCad.Text = Math.Round(lastSeenData2.cadence, 2).ToString() + " rpm";
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
